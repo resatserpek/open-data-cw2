136fe97cb9f94036c7128faa6812cf1797a9bfb6
ky1g17 <ky1g17@soton.ac.uk>
2021-05-24 22:13:16 +0100

    Clustering error fix later

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,12 +8,14 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import CitySelect from './CitySelect'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet'
 import axios from 'axios'
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import DatePicker from 'react-datepicker'
 import subDays from "date-fns/subDays";
 import "react-datepicker/dist/react-datepicker.css";
+import L from 'leaflet'
+
 
 
 
@@ -38,20 +40,52 @@ function ChangeView({ center, zoom }) {
 function App() {
   const [incidents, setIncidents] = useState([]);
   const [center, setCenter] = useState([50.9182575232534, -1.404944127424533]);
-  const [startDate, setStartDate] = useState(new Date());
+  const [zoom, setZoom] = useState(13);
+  const [cluster, setCluster] = useState(true);
+  const [startDate, setStartDate] = useState(new Date(parseInt('2021'), parseInt('02')));
   var dateRangeMax = new Date(parseInt('2021'), parseInt('02'))
   var dateRangeMin = new Date(parseInt('2018'), parseInt('04'))
 
+  const groupRef = useRef();
 
+
+  function MapEventComponent() {
+    const map = useMapEvents({
+      dragend: (e) => {
+        console.log("mapCenter", e.target.getCenter());
+        setCenter([e.target.getCenter().lat, e.target.getCenter().lng])
+        //setZoom(e.target.getZoom())
+        //console.log(e.target.getZoom());
+      },
+    });
+    return null;
+  }
+
+  function LayerEventComponent() {
+    const map = useMapEvents({
+      layeradd: (e) => {
+        console.log(e.target);
+      },
+    });
+    return null;
+  }
 
   useEffect(() => {
+    //setIncidents([]);
+    console.log(groupRef.current);
+    groupRef.current.markers.refreshClusters()
     axios.get(`https://data.police.uk/api/crimes-street/all-crime?lat=${center[0]}&lng=${center[1]}&date=${startDate.getFullYear()}-${startDate.getMonth() + 1}`)
       .then((response) => {
         console.log(response.data);
         setIncidents(response.data)
       });
+     
 
   }, [center, startDate]);
+
+  useEffect(() => {
+    setIncidents([])
+  }, [center])
 
   const handleSelect = (val) => {
     setCenter([val.latitude, val.longitude])
@@ -72,9 +106,6 @@ function App() {
               selected={startDate}
               onChange={startDate => setStartDate(startDate)}
               dateFormat="MM/yyyy"
-              // filterDate = {(date) =>{
-              //   return dateRangeMin < date < dateRangeMax
-              // }}
               maxDate={dateRangeMax}
               minDate={dateRangeMin}
               showMonthYearPicker
@@ -87,24 +118,26 @@ function App() {
 
 
 
-      <MapContainer center={center} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={center} zoom={13} scrollWheelZoom={true}  >
 
-        <ChangeView center={center} zoom={13} />
+        <ChangeView center={center} />
 
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <MarkerClusterGroup>
+       <MarkerClusterGroup removeOutsideVisibleBounds={true} ref={groupRef}>
 
           {incidents.map((incident, index) => {
             return (
               <CustomMarker key={index} latitude={incident.location.latitude} longitude={incident.location.longitude} category={incident.category} outcome={incident.outcome_status ? incident.outcome_status.category : "No outcome"} />
             )
           })}
-        </MarkerClusterGroup>;
-
+        </MarkerClusterGroup>
+       
+        <MapEventComponent/>
+        {/* <LayerEventComponent/> */}
       </MapContainer>
 
     </div>
